4b2f50e99d127a48915c88a5535b0bbd33ba941b
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-07 23:30:58 +0200

    t 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""setup flask-babel"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """confg lang and timezone """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Use request.accept_languages to
+    determine the best match with
+    our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """return simple home page"""
+    return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
