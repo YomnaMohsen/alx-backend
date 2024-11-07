a5c47306fdd8ee93a884202c04aa457213f2dc9e
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-07 23:21:37 +0200

    t 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""setup flask-babel"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """confg lang and timezone """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def home():
+    """return simple home page"""
+    return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
